d180ad30ebc804dc3e722e1ea571a6111bacfed4
Alisson <Alissonbpz@gmail.com>
2026-08-19 13:06:41 -0300

    Pagina de cadastro pronta

--- Aula_04_Tela_Cadastro/treino.py
+++ Aula_04_Tela_Cadastro/treino.py
@@ -7,12 +7,28 @@ def main(page:ft.Page):
 
     cabecalho = ft.Text("Cadastro", weight="bold", size=50, color="blue")
 
-    txt_confirmacao = ft.Text("", size= 30,color="green",weight="bold")
+    txt_confirmacao = ft.Text("", size= 18,color="green")
 
     def validacao(e):
-        if nome == "" or len(nome.value):
-            nome.error="Nome invalido, minimo de 3 caracteres."
-        nome.update()
+        if nome.value == "" or len(nome.value)<3:
+            nome.error="Nome invalido, minimo de 3 caracteres!!!"
+            nome.update()
+        else:
+            nome.error = None
+            nome.update()
+        if email.value == "" or "@" not in email.value:
+            email.error = "Email invalido!!!"
+            email.update()
+        else:
+            email.error = None
+            email.update()
+        if nome.error == None and email.error == None:
+            txt_confirmacao.value = "Cadastro realizado!"
+            txt_confirmacao.update()
+            nome.value = ""
+            email.value = ""
+            nome.update()
+            email.update()
 
     nome = ft.TextField(label="Digite seu nome: ", color ="yellow")
     email= ft.TextField(label="Email: ", color ="yellow")
