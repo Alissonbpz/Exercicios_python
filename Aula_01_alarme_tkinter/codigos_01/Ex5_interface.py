a1a7ea530324eff5fe720ff18468c8351e0bfb0a
Alisson <Alissonbpz@gmail.com>
2026-08-22 20:42:41 -0300

    Atualizando

--- Aula_01_alarme_tkinter/codigos_01/Ex5_interface.py
+++ Aula_01_alarme_tkinter/codigos_01/Ex5_interface.py
@@ -1,19 +1,10 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-def exibir_mensagem():
-    print("O botão foi clicado!")
-
-janela = tk.Tk()
-
-janela.title("posicionamento com Grid")
-
-rotulo = tk.Label(janela, text="Olá, Tkinter!")
-
-rotulo.grid(row=0, column=0, padx=10, pady=10) 
-
-botao = tk.Button(janela, text="Clique aqui", command=exibir_mensagem)
-botao.grid(row=1, column=0, padx=10, pady=10)
-
-
-janela.mainloop()
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
 
