13a53785200d1d9c7e0a4fd301fb973c394dcffc
Alisson <Alissonbpz@gmail.com>
2026-08-24 13:06:58 -0300

    Adicionando o botao de cadastro

--- Aula_04_Tela_Cadastro/teste.py
+++ Aula_04_Tela_Cadastro/teste.py
@@ -5,6 +5,8 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    txt_confirmacao = ()
+
     titulo = ft.Text("CADASTRO", font_family="comic sans", color="green", size=33, weight="Bold")
 
     nome = ft.TextField(
@@ -19,6 +21,15 @@ def main(page: ft.Page):
         label_style=ft.TextStyle(font_family="Times new roman")
     )
 
-    page.add(titulo, nome, email)
+    botao_cadastrar = ft.Button("CADASTRAR", style= ft.ButtonStyle(
+        shape = ft.RoundedRectangleBorder(radius=3),
+        bgcolor={
+            ft.ControlState.DEFAULT: "Blue", 
+            ft.ControlState.HOVERED: "Dark_Blue" 
+        },
+        color= "White"
+    ))
+
+    page.add(titulo, nome, email, botao_cadastrar)
 
 ft.run(main)
